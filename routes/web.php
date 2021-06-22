ca37940b08b102e5be5716a720ad056168cb5f5c
ducbl98 <ductit1998@gmail.com>
2021-06-22 10:47:28 +0700

    Laravel Register Validation

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::prefix('post')->group(function (){
+    Route::get('/create',[PostController::class,'create'])->name('post.create');
+    Route::post('/store',[PostController::class,'store'])->name('post.store');
+});
